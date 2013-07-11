cb7e9e78aea6df574974de8d1d8cd00fcf6a510c
Florent Jaby <fjaby@octo.com>
2013-07-11 17:46:58 +0200

    find 1

--- test/test-bisect.js
+++ test/test-bisect.js
@@ -1,5 +1,19 @@
 var bisect = require('../');
 
+exports['test find 1 in 0..10'] = function (test) {
+  var called = false;
+  var expected = 1;
+  test.expect(2 + 4); // 4 iterations expected
+  var actual = bisect(0, 10, function (index) {
+    test.ok(true, "We should get called");
+    called = true;
+    return index >= expected;
+  });
+  test.equal(expected, actual, "We should find 1 at index 1");
+  test.ok(called, "We should have called our predicate");
+  test.done();
+}
+
 exports['test find 4 in 0..10'] = function (test) {
   var called = false;
   var expected = 4;
